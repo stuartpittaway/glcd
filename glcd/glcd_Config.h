0c9316c22ac1f5dabd329dad5b0060b4cdcb446e
bperrybap@gmail.com <bperrybap@gmail.com@aa8516fa-6a64-7eeb-f22a-f70616c6bd37>
2010-01-27 06:59:56 +0000

    Most of the conditional defines have been removed as code was updated. all the config includes are now commented out. the idea is to put a private local include in the glcd_Config.h file. Both of us now have private areas under debug where we can place our own config files.

--- glcd/glcd_Config.h
+++ glcd/glcd_Config.h
@@ -17,13 +17,6 @@
 // up our minds on a final solution.
 // My thought is that all these "experimental" defines will go away.
 
-//#define CANNED_TAREAS	// defines up to the the first 4 text areas after #0 to be:
-						// Top, bottom, left, right halves of the screen.
-						// When GLCD_TAREA_CNT is less than 5 the areas are created in
-						// in the above order up to the GLCD_TAREA_CNT
-						//
-						// RECOMMENDED Setting: OFF
-
 
 //#define TRUE_WRITE	// does writes to memory on page crossings vs ORs
 						// When off, WriteData() works just like it used to.
@@ -47,61 +40,7 @@
 						// RECOMMENDED setting: OFF
 
 
-
-
-#define BITMAP_FIX		// erases region behind bitmap before painting
-						// It MUST be on if TRUE_WRITE is off.
-						// until there is a new bitmap rendering routine, this is a better
-						// method to use to fix bitmap rendering than TRUE_WRITE.
-						// Because if TRUE_WRITE is off, the bits will be ORd in and
-						// since the region behind the bitmap is erased, the ORs will or
-						// in the bitmap data properly on any boundary and the
-						// bitmap can be any height.
-						//
-						// TRUE_WRITE will render bitmaps on any boundary but not quite
-						// correctly. It will cause pixels below the bitmap to be cleared
-						// when the bitmap is not a multiple of 8 in height.
-						//
-						// this can be left on either way TRUE_WRITE is set.
-						// but isn't needed if TRUE_WRITE is set.
-						//
-						// RECOMMENDED setting: ON
-						
-						
-
-
-//#define SCROLL_WHITE	// always scroll in new WHITE pixels, rather than inverse of font color
-						// This also affects what pixel color is used on TWinClear().
-						// This only has effect when using the new PUTCHAR and must be used if
-						// TRUE_WRITE is off and NEW_FONTDRAW is also off
-						// it ensures the WHITE text works when using new putchar but the
-						// old rendering code.
-						//
-						// RECOMMENDED setting: OFF 
-
-
-#define LWRITE_FIX		// BUG fix in WriteData() - this should be a permanent fix
-						// This define turns on code to fix a bug in WriteData()
-						// WriteData() would attempt to write a byte below the
-						// bottom edge of the display when an unaligned byte write
-						// was done less than 8 pixels from the bottom.
-						// this corrects that
-						//
-						// RECOMMENDED setting: ALWAYS ON
-
 /*========================== End of experimental Defines ============================*/
-
-
-//#define ORIG_PUTCHAR			// revert back to pre wrap/scroll/window code
-								// also preserves old Puts newline behavior.
-								// also forces the old style character rendering.
-								// Basically a compability mode.
-								// Maybe this should be a V2_LIB Compability define?
-								// This would be useful for those that simply want to use
-								// the newer GLCD library and easier configuration
-								// but don't want any of the new stuff which might
-								// break their current code or eventually be a 
-								// different font format.
 
 /*
  * Defines below this point are only used with new putchar code.
@@ -125,9 +64,8 @@
 
  /*
   * the following list are the files to be included in the release 
-  * all other display specific files are in config/dev_Config.h
   */
-#include "dev_Config.h"  // temporary file that used to select actual config file
+
 //#include "config/ks0108_Config.h"
 //#include "config/sed1520_Config.h"
 
