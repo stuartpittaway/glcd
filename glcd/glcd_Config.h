7d96ef88310a6e436f30a9304ccf7a8ba244fd25
memargolis@gmail.com <memargolis@gmail.com@aa8516fa-6a64-7eeb-f22a-f70616c6bd37>
2010-03-13 14:17:04 +0000

    updated include files to those intended for initial release.
    Minor edits to some comments

--- glcd/glcd_Config.h
+++ glcd/glcd_Config.h
@@ -3,7 +3,7 @@
  *
  * vi:ts=8
  *
- * Use this file to configure which glcd module is used
+ * Use this file to select the active glcd configuration file
  * Then edit the selected module file to configure parameters for that module.
  *
 */
@@ -18,11 +18,15 @@
   *
  */
 
+#include "config/ks0108_Arduino_Config.h"     // config file for standard Arduino using documented wiring 
+//#include "config/ks0108_Mega_Config.h"      // config for Mega board using same pins as previous library
+//#include "config/ks0108_Sanguino_Config.h"  // config for Sanguino using same pins as previous library
 
-//#include "config/ks0108_Config.h"
-//#include "config/sed1520_Config.h"
 
-/*================================== User Defines ==================================*/
+//#include "config/sed1520_Config.h"          // generic SED1520 configuraition file 
+//#include "config/Modvk5121_Config.h"        // config file for vk5121 panel  
+
+/*========================== Optional User Defines ==================================*/
 
 //#define GLCD_NO_SCROLLDOWN	// disable reverse scrolling (saves ~600 bytes of code)
 				// This will allow those tight on FLASH space to save a bit of code space
@@ -33,7 +37,7 @@
 				// might have issues if a pin used shares a processor i/o port with an
 				// interrupt routine that updates pins/bits on the same port.
 
-//#define GLCD_OLD_FONTDRAW	// turn on to get old font rendering
+//#define GLCD_OLD_FONTDRAW	// uncomment this define to get old font rendering
 
 
 #endif
