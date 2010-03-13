8985d010bade33fdc27bb5639a05e62fc17291e5
memargolis@gmail.com <memargolis@gmail.com@aa8516fa-6a64-7eeb-f22a-f70616c6bd37>
2010-03-13 11:16:31 +0000

    aggregate header file that can include all fonts in the fonts directory
    in future this file could be automatically generated using fontcreater similar to the allBitmaps.h file

---
+++ glcd/fonts/allFonts.h
@@ -0,0 +1,13 @@
+/*
+ * allFonts.h font header for GLCD library 
+ * The fonts listed below will be available in a sketch if this file is included
+ *
+ * If you create your own fonts you can add the header to this file
+ *
+ * Note that the build environment only holds a font in Flash if its selected 
+ * so there is no penalty to including a font file here if its not used 
+ */
+
+#include "fonts/SystemFont5x7.h"       // system font
+#include "fonts/Arial14.h"             // proportional font
+#include "fonts/Verdana_digits_24.h"   // large font - numerals only 
