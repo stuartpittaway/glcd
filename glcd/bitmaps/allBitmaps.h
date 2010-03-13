65746470230d8a411b9d15657c37082e4415d814
memargolis@gmail.com <memargolis@gmail.com@aa8516fa-6a64-7eeb-f22a-f70616c6bd37>
2010-03-13 11:46:13 +0000

    aggregate header file that can include all bitmaps in the bitmaps directory
    This file is automatically generated using glcdMakeBitmap utility

---
+++ glcd/bitmaps/allBitmaps.h
@@ -0,0 +1,7 @@
+/* allBitmaps.h bitmap header for GLCD library */
+/* This file is created automatically by the glcdMakeBitmap utility */
+/* Any edits to this file will be lost when glcdMakeBitmap is next run */
+
+#include "ArduinoIcon32x32.h"
+#include "ArduinoIcon64x32.h"
+#include "ArduinoIcon64x64.h"
